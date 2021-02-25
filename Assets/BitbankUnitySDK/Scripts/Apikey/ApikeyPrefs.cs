94bc4cc56026287c15f030da42b8a521db2d684b
DESKTOP-2VGTF7G\taku <keep_slimbody@yahoo.co.jp>
2021-02-25 13:10:18 +0900

    Save Unity ApiKys

--- Assets/BitbankUnitySDK/Scripts/Apikey/ApikeyPrefs.cs
+++ Assets/BitbankUnitySDK/Scripts/Apikey/ApikeyPrefs.cs
@@ -10,29 +10,8 @@ namespace BitbankUnitySDK
     {
         private const string EncyptPassword = "+jW:f`4?6p)RG^Qq6M|g0Nh2r|jGke)k";
 
-        public string access_token;
-        public string token_type;
-        public string refresh_token;
-        public string scope;
-        public long expires_in;
-        public long created_at;
-
-        public DateTime CreatedAtDateTime()
-        {
-            DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            return UNIX_EPOCH.AddSeconds(created_at).ToLocalTime();
-        }
-
-        public DateTime ExpiresAtDateTime()
-        {
-            DateTime createdAt = CreatedAtDateTime();
-            return createdAt.AddSeconds(expires_in).ToLocalTime();
-        }
-
-        public bool IsExired()
-        {
-            return DateTime.Now > ExpiresAtDateTime();
-        }
+        public string apikey;
+        public string secret;
 
         public static void WriteFile(ApikeyPrefs account)
         {
---
+++ Assets/Examples/Scripts/ApiKeyForm.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BitbankUnitySDK
+{
+    public class ApiKeyForm : MonoBehaviour
+    {
+        [SerializeField] private InputField apiKeyInputField;
+        [SerializeField] private InputField secretInputField;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnSaveButtonClicked()
+        {
+            ApikeyPrefs apiKeyPref = new ApikeyPrefs();
+            apiKeyPref.apikey = apiKeyInputField.text;
+            apiKeyPref.secret = secretInputField.text;
+            ApikeyPrefs.WriteFile(apiKeyPref);
+        }
+    }
+}
